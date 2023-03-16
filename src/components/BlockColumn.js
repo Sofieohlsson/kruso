d9895dba9c6eadb1ab5b264df1607bdc4ae37ebe
Sofie <anna.sofie.ohlsson@gmail.com>
2023-03-16 16:20:27 +0000

    styling

---
+++ src/components/BlockColumn.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const BlockColumn = ({ column }) => {
+    return (
+        <div className="blockcontainer" style={{backgroundImage:`url("${column.image}")` }} >
+            {(column.heading) && (
+                <h1 className="blockheading">{column.heading}</h1>
+            )}
+
+
+            <div className="blockinfo">
+                <p>{column.text}</p>
+                {(column.link) && (
+                    <Link to={column.link} className="blocklink">Read more</Link>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default BlockColumn; 
--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import api from "../services/api";
-import BlockItems from "./BlockItems";
+import BlockColumn from "./BlockColumn";
 
 const Blocks = () => {
     const [blocks, setBlocks] = useState([]);
@@ -13,23 +13,23 @@ const Blocks = () => {
 
     }, [])
 
-
-    // const columns = blocks[0].columns[0].heading; 
-
     return (
-        <div>
-            <div>Vi funkar</div>
-            <div>
-                {blocks.map((data, index) => {
-                    if (data.type === "columns") {
-                        data.columns.map((heading, text, link, image) => {
-                            return(
-                                <BlockItems heading={heading} text={text} link={link} image={image} key={index}/>
-                            )
-                        })
-                    }
-                })}
-            </div>
+        <div className="blockwrapper">
+            {blocks.map((data, index) => {
+                return (
+                    <>
+                        {(data.type === "columns") && (
+                            <div className={data.columns.length > 1 ? "blockbox" : "blockpadding"}>
+                                {data.columns.map((column, index) => {
+                                    return (
+                                        <BlockColumn column={column} key={index} />
+                                    )
+                                })}
+                            </div>
+                        )}
+                    </>
+                )
+            })}
         </div>
     )
 }
